c346fcaacc46b7e017c62a0056790b85f7c147be
meir_p <meir12345p@gmail.com>
2026-02-24 15:16:45 +0200

    fix all

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,18 @@ import "./App.css";
 import Timer from "./components/Timer";
 import Header from "./components/Header";
 import Board from "./components/Board";
-import './style/Header.css'
+import "./style/Header.css";
 import { ContextProvider } from "./Provider/ContextProvider";
 
-
-
 function App() {
-  return <>
-    <ContextProvider>
-      <Header></Header>
-      <Timer></Timer>
-      <Board />
-    </ContextProvider>
-  </>
-
-
-
+  return (
+    <>
+      <ContextProvider>
+        <Header></Header>
+        <Timer></Timer>
+        <Board />
+      </ContextProvider>
+    </>
+  );
+}
 export default App;
--- src/Provider/ContextProvider.jsx
+++ src/Provider/ContextProvider.jsx
@@ -1,21 +1,31 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
-
-const ThemeProvider = createContext()
+const ThemeProvider = createContext();
 function ContextProvider({ children }) {
-    const [cubeList, setCubeList] = useState([])
-    const [bomb, setBomb] = useState(9)
-    const [minutes, setMinutes] = useState(1);
-    const [seconds, setSeconds] = useState(0);
-    const [time, setTime] = useState(true);
+  const [cubeList, setCubeList] = useState([]);
+  const [bomb, setBomb] = useState(10);
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(0);
+  const [time, setTime] = useState(true);
 
-
-
-    return (
-        <ThemeProvider value={{ cubeList, setCubeList, bomb, setBomb, minutes, setMinutes, seconds, setSeconds, time, setTime }}>
-            {children}
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider
+      value={{
+        cubeList,
+        setCubeList,
+        bomb,
+        setBomb,
+        minutes,
+        setMinutes,
+        seconds,
+        setSeconds,
+        time,
+        setTime,
+      }}
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
 
-export {ContextProvider, ThemeProvider}
+export { ContextProvider, ThemeProvider };
