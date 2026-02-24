66e1f1b2551e387aafc8a150eb1ed64a4659debe
hagay kohelet <hagay2506@gmail.com>
2026-02-24 12:51:35 +0200

    finish board

--- src/components/cube.jsx
+++ src/components/cube.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../style/cube.css'
-function Cube({name}) {
+function Cube({onClick}) {
   return (
-    <div className='cube'>{name}</div>
+    <div className='cube' onClick={onClick}></div>
   )
 }
 
